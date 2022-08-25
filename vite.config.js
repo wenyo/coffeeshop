85e3f7100184eac2f61e4fe5a97e8c007945c9b3
wen yo <wenda897436@gmail.com>
2022-08-25 21:26:50 +0800

    fix path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === "production" ? "/coffeeshop/" : "/",
+  base: "./",
   css: {
     preprocessorOptions: {
       scss: {
